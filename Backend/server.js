e8d58e5203fa2a212fdae743e46d7689b81ea392
Abdo Khattab <mr.spykhattab@gmail.com>
2024-05-14 22:22:45 +0300

    backend init commit

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+
+// Adding config variables to node process
+require("dotenv").config({ path: "./config.env" });
+
+const connectionString = process.env.CONNECTION_STRING.replace(
+  "<PASSWORD>",
+  process.env.PASSWORD
+);
+
+// Connect to mongoDB server via mongoose
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB", err);
+  });
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
