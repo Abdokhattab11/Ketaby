a06d3c3c7e32183bb6bbfbd4f3cadc12eea1f68c
Abdo Khattab <mr.spykhattab@gmail.com>
2024-05-14 23:01:24 +0300

    Added CROS

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with frontend origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
   res.send("Hello from the backend");
 });
