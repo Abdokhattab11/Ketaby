370eac00ca330d6d7be629b3388c88941bed7d95
Abdo Khattab <mr.spykhattab@gmail.com>
2024-05-17 19:58:20 +0300

    Modified AuthorContollers.js

--- Backend/controllers/authorControllers.js
+++ Backend/controllers/authorControllers.js
@@ -1,3 +1,5 @@
+const authorModel = require("../models/authorModel");
+
 /**
  * Get All Authors from the database and filter them by the query parameters
  * @param {*} req
@@ -5,7 +7,24 @@
  * @QueryParams  rating, nationality
  * @returns {Array} authors
  */
-exports.getAllAuthors = async (req, res) => {};
+exports.getAllAuthors = async (req, res) => {
+  try {
+    const authors = await authorModel.find();
+    if (!authors) {
+      throw new Error("No authors found");
+    }
+    res.status(200).json({
+      status: "success",
+      lengthOfData: authors.length,
+      data: authors,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
 
 /**
  * Get Author by Id and if not exists return 404
@@ -13,7 +32,15 @@ exports.getAllAuthors = async (req, res) => {};
  * @param {*} res
  * @returns {Object} author
  */
-exports.getAuthorById = async (req, res) => {};
+exports.getAuthorById = async (req, res) => {
+  try {
+    author = await authorModel.findOne({ author_id: req.params.id });
+    if (!author) throw new Error("No author found");
+    res.status(200).json({ status: "success", data: author });
+  } catch (err) {
+    res.status(404).json({ status: "fail", message: err.message });
+  }
+};
 
 /**
  * Create new Author
@@ -22,16 +49,28 @@ exports.getAuthorById = async (req, res) => {};
  * @QueryParams id
  * @returns {Object} author
  */
-exports.createAuthor = async (req, res) => {};
-
-/**
- * Delete an Author by Id and if not exists return 404
- * @param {*} req
- * @param {*} res
- * @QueryParams id
- * @returns {Object} author
- */
-exports.deleteAuthor = async (req, res) => {};
+exports.createAuthor = async (req, res) => {
+  try {
+    const newAuthor = await authorModel.create(req.body);
+    res.status(201).json({
+      status: "success",
+      data: newAuthor,
+    });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err.message,
+    });
+  }
+};
+exports.deleteAuthor = async (req, res) => {
+  try {
+    await authorModel.findByIdAndDelete(req.params.author_id);
+    res.status(200);
+  } catch (e) {
+    res.send(404).json({ massage: e });
+  }
+};
 
 /**
  * Update an Author by Id and if not exists return 404
@@ -40,4 +79,14 @@ exports.deleteAuthor = async (req, res) => {};
  * @param {Object} req.Body
  * @returns {Object} author
  */
-exports.updateAuthor = async (req, res) => {};
+exports.updateAuthor = async (req, res) => {
+  try {
+    const author = await authorModel.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      status: "success",
+      data: author,
+    });
+  } catch (err) {
+    res.send(404).json({ status: "fail", message: err.message });
+  }
+};
